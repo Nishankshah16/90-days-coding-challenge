80b8358e4d31c4f205b6cd31c6613c7346740bcf
nishank <nishankshah16@gmail.com>
2025-01-03 16:41:20 +0530

    DAY7

---
+++ DAY7/hangman.py
@@ -0,0 +1,41 @@
+import Hangmanart as Hangmanart
+import random
+
+
+
+lives=6
+assigned_word= random.choice(Hangmanart.word).lower()
+
+placeholder=""
+for position in range(0,len(assigned_word)):
+    placeholder+="_"
+print(placeholder)
+
+correct_letters=[]
+game_over=False
+
+while not game_over:
+    guess= input("Guess the letter : " ).lower()[0]
+    display=""
+    for letter in assigned_word:
+        if letter == guess:
+            display +=letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display+=letter
+        else:
+            display+="_"
+
+    print(display)
+    print(Hangmanart.HANGMANPICS[lives])
+
+    if "_" not in display:
+        game_over=True
+        print("you win")
+    elif guess not in assigned_word:
+        lives-=1
+        if lives==0:
+            game_over=True
+            print("You lose")
+    
+   

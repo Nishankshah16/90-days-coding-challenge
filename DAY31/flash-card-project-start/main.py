ed56c98919b7d8179e0d0bc2a3598dd059a4d508
Nishankshah16 <77563104+Nishankshah16@users.noreply.github.com>
2025-03-06 10:06:17 +0530

    DAY31

--- DAY31/flash-card-project-start/main.py
+++ DAY31/flash-card-project-start/main.py
@@ -1,20 +1,28 @@
 BACKGROUND_COLOR = "#B1DDC6"
+list1={}
 
 from tkinter import *
 import pandas as pd
 import random as r
+
 windows=Tk()
 windows.title("Flashy")
 windows.config(padx=50, pady=50, background=BACKGROUND_COLOR, highlightthickness=0)
 # windows.minsize(500,500)
 #--------------------------------------------------------------------------
 
-df=pd.read_csv("/Users/nishanknitinshah/Documents/Python/90 DAYS OF PYTHON/DAY31/flash-card-project-start/data/french_words.csv")
-list1 = df.to_dict(orient="records")
+try:
+    data=pd.read_csv("/Users/nishanknitinshah/Documents/Python/90 DAYS OF PYTHON/DAY31/flash-card-project-start/data/Words_to_learn.csv")
+except FileNotFoundError:
+    df=pd.read_csv("/Users/nishanknitinshah/Documents/Python/90 DAYS OF PYTHON/DAY31/flash-card-project-start/data/french_words.csv")
+    list1 = df.to_dict(orient="records")
+else:
+    list1 = df.to_dict(orient="records")
 card = {}
 
 def generate_word():
     global card, timer
+    windows.after_cancel(timer)
     card=r.choice(list1)
     canvas.itemconfig(title, text="French", fill="black")
     canvas.itemconfig(word, text=card["French"], fill="black")
@@ -28,6 +36,11 @@ def flip():
     canvas.itemconfig(canvas_img, image=card_back_image)
 
 
+def isknown():
+    list1.remove(card)
+    data=pd.DataFrame(list1)
+    data.to_csv("/Users/nishanknitinshah/Documents/Python/90 DAYS OF PYTHON/DAY31/flash-card-project-start/data/Words_to_learn.csv")
+    generate_word()
 timer = windows.after(3000, flip)
 
 
@@ -53,7 +66,7 @@ canvas.grid(row=0, column=0, columnspan=2)
 
 #buttons
 right_button_image=PhotoImage(file="/Users/nishanknitinshah/Documents/Python/90 DAYS OF PYTHON/DAY31/flash-card-project-start/images/right.png")
-right_button=Button(image=right_button_image,highlightthickness=0,background=BACKGROUND_COLOR,command=generate_word)
+right_button=Button(image=right_button_image,highlightthickness=0,background=BACKGROUND_COLOR,command=isknown)
 right_button.grid(row=1, column=1)
 
 left_button_image=PhotoImage(file="/Users/nishanknitinshah/Documents/Python/90 DAYS OF PYTHON/DAY31/flash-card-project-start/images/wrong.png")

59b85588381a559fe2a9f4c47eb0a5f569b0fc24
Nishankshah16 <77563104+Nishankshah16@users.noreply.github.com>
2025-03-22 22:09:10 +0530

    DAY 38

--- DAY37/main.py
+++ DAY37/main.py
@@ -1,10 +1,13 @@
 import requests
+import datetime as dt
 
 pixel_endpoint="https://pixe.la/v1/users"
-
+user_name="nishank16"
+token= "NISHANKSHAH"
+graph_id="nishank1"
 users_parms={
-    "token":"NISHANKSHAH",
-    "username":"nishank16",
+    "token":token,
+    "username":user_name,
     "agreeTermsOfService":"yes",
     "notMinor":"yes"
     
@@ -13,3 +16,42 @@ users_parms={
 
 # response= requests.post(url= pixel_endpoint, json=users_parms)
 # print(response.text)
+
+graph_endpoint= f"{pixel_endpoint}/{user_name}/graphs"
+
+graph_config={
+    "id": graph_id,
+    "name": "nishank_graph",
+    "unit":"Km",
+    "type":"float",
+    "color":"ajisai"
+}
+
+headers={
+    "X-USER-TOKEN":token
+}
+
+# response= requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+today_date=dt.datetime(year=2025, month=3, day=22)
+print(today_date)
+
+pixel_endpoint= f"{pixel_endpoint}/{user_name}/graphs/{graph_id}"
+
+pixel_data={
+    "date" : today_date.strftime('%Y%m%d'),
+    "quantity" : "11.74",
+}
+
+# response= requests.post(url=pixel_endpoint, json=pixel_data, headers=headers)
+# print(response.text)
+
+pixel_update = f"{pixel_endpoint}/{user_name}/graphs/{graph_id}/{today_date}"
+pixel_update_data={
+    "quantity" : "11.74",
+}
+
+response=requests.put(url = pixel_update, json = pixel_update_data, headers=headers)
+print(response.text)
+
+# /v1/users/<username>/graphs/<graphID>/<yyyyMMdd>
---
+++ DAY38/main.py
@@ -0,0 +1,56 @@
+# nutition_api= https://www.nutritionix.com/business/api
+
+import requests
+import datetime as dt
+
+api_key="9eb5cbfc27e07c3fb75f6f45794de819"
+api_id="372eece0"
+# GENDER = "male"
+WEIGHT_KG = 65
+HEIGHT_CM = 173
+AGE = 27
+
+API_ENDPOINT="https://trackapi.nutritionix.com/v2/natural/exercise"
+input_exercise=input("Please tell what you did today: ")
+headers={
+    "x-app-id": api_id,
+    "x-app-key": api_key,
+    "Content-Type": "application/json"
+}
+
+parameters={
+    "query": input_exercise,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response=requests.post(url=API_ENDPOINT, json= parameters, headers=headers)
+data=response.json()
+print(data)
+
+
+
+today_date = dt.datetime.now().strftime("%d/%m/%Y")
+now_time = dt.datetime.now().strftime("%X")
+
+
+username="3550a86d20c174db2e94b4b24276862b"
+project="myWorkoutsRegister"
+sheetname="workouts"
+sheet_endpoint=f"https://api.sheety.co/{username}/{project}/{sheetname}"
+
+for exercise in data["exercises"]:
+    sheet_input={
+        "workout": {
+        "date": today_date,
+        "time": now_time,
+        "exercise": exercise["name"].title(),
+        "duration": exercise["duration_min"],
+        "calories": exercise["nf_calories"]
+        }
+    }
+    response=requests.post(url=sheet_endpoint,json=sheet_input)
+    print(response.text)
+
+
